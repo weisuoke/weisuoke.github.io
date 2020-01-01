2c10bf158ff7f8de4915213e2d3bde885362442f
梅林的法杖 <530264127@qq.com>
2020-01-01 22:16:23 +0800

    wxx

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,265 +1,272 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
-  title: '邬晓’s blog',
-  description: 'Redesign Everything',
+  title: "邬晓’s blog",
+  description: "Redesign Everything",
   themeConfig: {
     search: false,
     nav: [
-      { text: '主页', link: '/' },
-      { 
-        text: '前端精进手册', 
-        link: '/fe/',
+      { text: "主页", link: "/" },
+      {
+        text: "前端精进手册",
+        link: "/fe/",
         items: [
-          {text: 'React', link: '/fe/react/'},
-          {text: 'Vue', link: '/fe/vue/'},
-          {text: 'Webpack', link: '/fe/webpack/'},
-        ]
-      },
-      { 
-        text: '算法学习', 
-        link: '/algorithm/',
-        items: [
-          {text: '数据结构', link: '/algorithm/ds/'},
-          {text: '算法', link: '/algorithm/ag/'}
+          { text: "React", link: "/fe/react/" },
+          { text: "Vue", link: "/fe/vue/" },
+          { text: "Webpack", link: "/fe/webpack/" }
         ]
       },
       {
-        text: '玩转Github',
-        link: '/github/',
+        text: "算法学习",
+        link: "/algorithm/",
         items: [
-          {text: '我的github', link: '/github/my-github'},
+          { text: "数据结构", link: "/algorithm/ds/" },
+          { text: "算法", link: "/algorithm/ag/" }
         ]
       },
-      { text: '效率', link: '/effective/' },
-      { text: 'WIKI', link: '/wiki/' },
-      { text: 'FLAG', link: '/flag/' },
-      { text: 'ABOUT ME', link: '/about/' },
-      { text: 'WUXIAO.IO', link: 'https://wuxiao.io' },
-      { text: 'WUXIAO.CODES', link: 'https://react.wuxiao.codes' }
+      {
+        text: "玩转Github",
+        link: "/github/",
+        items: [{ text: "我的github", link: "/github/my-github" }]
+      },
+      { text: "效率", link: "/effective/" },
+      { text: "读书笔记", link: "/reading/" },
+      { text: "WIKI", link: "/wiki/" },
+      { text: "FLAG", link: "/flag/" },
+      { text: "ABOUT ME", link: "/about/" },
+      { text: "WUXIAO.IO", link: "https://wuxiao.io" },
+      { text: "WUXIAO.CODES", link: "https://react.wuxiao.codes" }
     ],
-    lastUpdated: '更新时间',
+    lastUpdated: "更新时间",
     sidebar: [
       {
-        title: '前端精进手册',   // 必要的
-        path: '/fe/',      // 可选的, 应该是一个绝对路径
+        title: "前端精进手册", // 必要的
+        path: "/fe/", // 可选的, 应该是一个绝对路径
         collapsable: true, // 可选的, 默认值是 true,
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        sidebarDepth: 1, // 可选的, 默认值是 1
         children: [
           {
-            title: 'CSS', 
-            path: '/fe/css/', 
+            title: "CSS",
+            path: "/fe/css/",
             collapsable: true,
-            sidebarDepth: 2,  
-            children: [
-              '/fe/css/xFC',
-              '/fe/css/center-div'
-            ]
+            sidebarDepth: 2,
+            children: ["/fe/css/xFC", "/fe/css/center-div"]
           },
           // ES6
           {
-            title: 'ES6+', 
-            path: '/fe/es6/', 
+            title: "ES6+",
+            path: "/fe/es6/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2,
             children: [
-              '/fe/es6/var-let-const',
-              '/fe/es6/arrow-function',
-              '/fe/es6/set-map',
-              '/fe/es6/generator',
-              '/fe/es6/async',
-              '/fe/es6/decorate',
-              '/fe/es6/proxy'
+              "/fe/es6/var-let-const",
+              "/fe/es6/arrow-function",
+              "/fe/es6/set-map",
+              "/fe/es6/generator",
+              "/fe/es6/async",
+              "/fe/es6/decorate",
+              "/fe/es6/proxy"
             ]
           },
           // 手写代码
           {
-            title: '手写代码系列', 
-            path: '/fe/js-implement/', 
+            title: "手写代码系列",
+            path: "/fe/js-implement/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2,
             children: [
-              '/fe/js-implement/call.md',
-              '/fe/js-implement/bind.md',
-              '/fe/js-implement/debounce.md',
-              '/fe/js-implement/throttle.md',
-              '/fe/js-implement/instanceof.md',
-              '/fe/js-implement/reduce.md',
-              '/fe/js-implement/new.md',
-              '/fe/js-implement/aop.md',
-              '/fe/js-implement/curry.md',
-              '/fe/js-implement/JSON.stringify.md',
-              '/fe/js-implement/JSON.parse.md',
-              '/fe/js-implement/deepClone.md',
-              '/fe/js-implement/Symbol.md',
-              '/fe/js-implement/flatten.md',
-              '/fe/js-implement/unique.md',
-              '/fe/js-implement/Promise',
-              '/fe/js-implement/jsonp',
-              '/fe/js-implement/my-webpack'
+              "/fe/js-implement/call.md",
+              "/fe/js-implement/bind.md",
+              "/fe/js-implement/debounce.md",
+              "/fe/js-implement/throttle.md",
+              "/fe/js-implement/instanceof.md",
+              "/fe/js-implement/reduce.md",
+              "/fe/js-implement/new.md",
+              "/fe/js-implement/aop.md",
+              "/fe/js-implement/curry.md",
+              "/fe/js-implement/JSON.stringify.md",
+              "/fe/js-implement/JSON.parse.md",
+              "/fe/js-implement/deepClone.md",
+              "/fe/js-implement/Symbol.md",
+              "/fe/js-implement/flatten.md",
+              "/fe/js-implement/unique.md",
+              "/fe/js-implement/Promise",
+              "/fe/js-implement/jsonp",
+              "/fe/js-implement/my-webpack"
             ]
           },
           // 原理系列
           {
-            title: '原理系列', 
-            path: '/fe/js-theory/', 
+            title: "原理系列",
+            path: "/fe/js-theory/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2,
             children: [
-              '/fe/js-theory/code-fragment-analysis',
-              '/fe/js-theory/type-of-js',
-              '/fe/js-theory/js-scope',
-              '/fe/js-theory/memory',
-              '/fe/js-theory/asynchronous',
-              '/fe/js-theory/event',
-              '/fe/js-theory/event-loop',
-              '/fe/js-theory/is-virtual-dom-fast',
-              '/fe/js-theory/executor-stack',
-              '/fe/js-theory/how-to-implement-a-router',
-              '/fe/js-theory/equal-different',
-              '/fe/js-theory/service-worker-life-cycle',
-              '/fe/js-theory/browser-cache',
-              '/fe/js-theory/browser-rendering',
-              '/fe/js-theory/web-components',
-              '/fe/js-theory/webassembly',
-              '/fe/js-theory/npm',
-              '/fe/js-theory/websocket',
-              '/fe/js-theory/Immutable'
+              "/fe/js-theory/code-fragment-analysis",
+              "/fe/js-theory/type-of-js",
+              "/fe/js-theory/js-scope",
+              "/fe/js-theory/memory",
+              "/fe/js-theory/asynchronous",
+              "/fe/js-theory/event",
+              "/fe/js-theory/event-loop",
+              "/fe/js-theory/is-virtual-dom-fast",
+              "/fe/js-theory/executor-stack",
+              "/fe/js-theory/how-to-implement-a-router",
+              "/fe/js-theory/equal-different",
+              "/fe/js-theory/service-worker-life-cycle",
+              "/fe/js-theory/browser-cache",
+              "/fe/js-theory/browser-rendering",
+              "/fe/js-theory/web-components",
+              "/fe/js-theory/webassembly",
+              "/fe/js-theory/npm",
+              "/fe/js-theory/websocket",
+              "/fe/js-theory/Immutable"
             ]
           },
           {
-            title: 'Webpack', 
-            path: '/fe/webpack/', 
+            title: "Webpack",
+            path: "/fe/webpack/",
             collapsable: true,
-            sidebarDepth: 2,  
-            children: [
-              '/fe/webpack/Webpack基础',
-              '/fe/webpack/手写Webpack'
-            ]
+            sidebarDepth: 2,
+            children: ["/fe/webpack/Webpack基础", "/fe/webpack/手写Webpack"]
           },
           {
-            title: 'TypeScript', 
-            path: '/fe/typescript/', 
+            title: "TypeScript",
+            path: "/fe/typescript/",
             collapsable: true,
-            sidebarDepth: 2,  
-            children: [
-              '/fe/typescript/docs'
-            ]
+            sidebarDepth: 2,
+            children: ["/fe/typescript/docs"]
           },
           // React
           {
-            title: 'React', 
-            path: '/fe/react/', 
+            title: "React",
+            path: "/fe/react/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2,
             children: [
-              '/fe/react/01.React必知必会',
-              '/fe/react/t01.React-TypeScript'
+              "/fe/react/01.React必知必会",
+              "/fe/react/t01.React-TypeScript"
             ]
           },
           // Vue
           {
-            title: 'Vue', 
-            path: '/fe/Vue/', 
+            title: "Vue",
+            path: "/fe/Vue/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2,
             children: [
-              '/fe/Vue/01.vue-need-to-know',
-              '/fe/Vue/Vue-Basic',
-              '/fe/Vue/Vue-Components',
-              '/fe/Vue/Vue-filter-directive',
-              '/fe/Vue/Vue-SSR'
+              "/fe/Vue/01.vue-need-to-know",
+              "/fe/Vue/Vue-Basic",
+              "/fe/Vue/Vue-Components",
+              "/fe/Vue/Vue-filter-directive",
+              "/fe/Vue/Vue-SSR"
             ]
           },
           {
-            title: 'PWA', 
-            path: '/fe/PWA/', 
+            title: "PWA",
+            path: "/fe/PWA/",
             collapsable: true,
-            sidebarDepth: 2,  
-            children: [
-              '/fe/PWA/pwa'
-            ]
+            sidebarDepth: 2,
+            children: ["/fe/PWA/pwa"]
           },
           // 微信小程序
           {
-            title: '微信小程序', 
-            path: '/fe/weapp/', 
+            title: "微信小程序",
+            path: "/fe/weapp/",
             collapsable: true,
-            sidebarDepth: 2,  
-            children: [
-            ]
+            sidebarDepth: 2,
+            children: []
           },
           // 网络
           {
-            title: '网络相关', 
-            path: '/fe/http/', 
+            title: "网络相关",
+            path: "/fe/http/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2,
             children: [
-              '/fe/http/get-post', 
-              '/fe/http/http', 
-              '/fe/http/http2', 
-              '/fe/http/tcp-connection', 
-              '/fe/http/from-url-to-page', 
+              "/fe/http/get-post",
+              "/fe/http/http",
+              "/fe/http/http2",
+              "/fe/http/tcp-connection",
+              "/fe/http/from-url-to-page"
             ]
           },
           // 性能优化
           {
-            title: '性能', 
-            path: '/fe/optimize/', 
+            title: "性能",
+            path: "/fe/optimize/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2,
             children: [
-              '/fe/optimize/optimize-need-to-know', 
-              '/fe/optimize/monitor', 
+              "/fe/optimize/optimize-need-to-know",
+              "/fe/optimize/monitor"
             ]
           },
           // 设计模式
           {
-            title: '设计模式', 
-            path: '/fe/Gof/', 
+            title: "设计模式",
+            path: "/fe/Gof/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2
           },
           // 前端安全
           {
-            title: '前端安全', 
-            path: '/fe/security/', 
+            title: "前端安全",
+            path: "/fe/security/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2
           }
         ]
       },
       {
-        title: '算法学习',
-        path: '/algorithm/',
+        title: "前端精进2020",
+        path: "/fe2020/",
         collapsable: true,
-        sidebarDepth: 1, 
+        sidebarDepth: 1
+      },
+      {
+        title: "读书笔记",
+        path: "/reading/",
+        collapsable: true,
+        sidebarDepth: 1,
+        children: [
+          {
+            title: "思维",
+            path: "/reading/thinking/",
+            collapsable: true,
+            sidebarDepth: 2,
+            children: ["/reading/thinking/anshijian"]
+          }
+        ]
+      },
+      {
+        title: "算法学习",
+        path: "/algorithm/",
+        collapsable: true,
+        sidebarDepth: 1,
         children: [
           // 数据结构
           {
-            title: '数据结构', 
-            path: '/algorithm/ds/', 
+            title: "数据结构",
+            path: "/algorithm/ds/",
             collapsable: true,
-            sidebarDepth: 2,  
+            sidebarDepth: 2,
             children: [
-              '/algorithm/ds/Stack',
-              '/algorithm/ds/LinkedList',
-              '/algorithm/ds/Tree',
-              '/algorithm/ds/Heap'
+              "/algorithm/ds/Stack",
+              "/algorithm/ds/LinkedList",
+              "/algorithm/ds/Tree",
+              "/algorithm/ds/Heap"
             ]
           },
           // 算法
           {
-            title: '算法', 
-            path: '/algorithm/ag/', 
+            title: "算法",
+            path: "/algorithm/ag/",
             collapsable: true,
-            sidebarDepth: 2,  
-            children: [
-              '/algorithm/ag/sort'
-            ]
+            sidebarDepth: 2,
+            children: ["/algorithm/ag/sort"]
           }
         ]
       }
     ]
   }
-}
+};
--- docs/README.md
+++ docs/README.md
@@ -6,8 +6,7 @@ features:
   details: 997是一种态度，前端路漫漫，生死看淡，不服就干。
 - title: 极简主义
   details: Less is more
-- title: 骑行爱好者
-  details: 爱你，恨你就带你走318
+- title: 时间就是金钱，效率就是生命
 # footer: <a href="http://www.baidu.com">赣ICP备17007608号-2</a> | Vuepress搭建
 ---
 
